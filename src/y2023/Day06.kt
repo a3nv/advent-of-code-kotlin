e3cb948b3fc9bd1c9fb7060c6b0185447aeb5e54
a3nv <asonovivan88@gmail.com>
2023-12-11 23:12:18 +0100

    Advent of kotlin. Day 6. Part 2.
    - improved solution, instead of calculating each speed and it's distance
      in the range, we're solving standart quadratic equation to find a range
    in which covered distances will be greater than given distance.
    
    Here is an explanation:
    The equation we're dealing with here is speed * (time - speed). This equation represents a parabola, which is a graph that is shaped like a U or an upside-down U. The parabola you're dealing with in this case is upside-down because of the negative sign between speed and time - speed.
    
    A parabola is symmetrical, meaning it looks the same on both sides. The highest or lowest point of a parabola is called its vertex, and the x-coordinate of this vertex is time / 2 in your case.
    
    The calculation val maxDistance = (maxSpeed * (t - maxSpeed)).toLong() finds the maximum distance you can cover, which is the y-coordinate of the vertex of this parabola. If this maximum distance is greater than your actual distance d, then there must be some lower and higher speeds such that their corresponding distances are exactly d.
    
    The calculation val sqrtPart = Math.sqrt((t * t / 4) - d) is based on solving the equation speed * (time - speed) = d for speed. This equation is a quadratic equation, and solving it involves computing a square root, hence the Math.sqrt part.
    
    Finally, val lowSpeed = Math.ceil(t / 2 - sqrtPart).toLong() and val highSpeed = Math.floor(t / 2 + sqrtPart).toLong() compute the lowest and highest speeds, respectively, such that the covered distance is greater than d. This range of speeds is added to the counter number, because any speed within this range will result in a covered distance that meets your requirement.

--- src/y2023/Day06.kt
+++ src/y2023/Day06.kt
@@ -1,31 +1,37 @@
 package y2023
 
 import utils.readInput
+import kotlin.math.ceil
+import kotlin.math.floor
+import kotlin.math.sqrt
 
 fun main() {
 
 
+    /**
+     * Equation speed * (time - speed) represents parabola. In this case upside-down U shaped because time - speed is a negative value.
+     */
     fun part1(input: List<String>): Long {
         val times = input.first().substringAfter("Time:").split(" ")
             .filter { it.isNotBlank() }.map { it.trim().toLong() }
         val distances = input.last().substringAfter("Distance:").split(" ")
             .filter { it.isNotBlank() }.map { it.trim().toLong() }
         val pairs = times.zip(distances)
-        println(times)
-        println(distances)
-        println(pairs)
         var number = 1L
         pairs.forEach {
-            var counter = 0L
-            for (i in 1 .. it.first) {
-                val speed = i
-                val distance = speed * (it.first - i)
-                println("with speed $i it will go for ${it.first - i} and will cover $distance")
-                if (distance > it.second) {
-                    counter++
-                }
+            val t = it.first.toDouble()
+            val d = it.second
+            val maxSpeed = t / 2 // since it's a parabola maxSpeed will be reached at the middle of time
+            val maxDistance = (maxSpeed * (t - maxSpeed)).toLong() // max distance, which is y-coordinate.
+            if (maxDistance > d) { // if it's greater than distance d then there must be lower and higher speeds that their corresponding distances are exactly d.
+                val sqrtPart = sqrt((t * t / 4) - d) // this is the root of a standard quadratic equation 'speed * (time - speed) = d' for speed
+                val lowSpeed = ceil(t / 2 - sqrtPart).toLong() // finding roots of the equation
+                val highSpeed = floor(t / 2 + sqrtPart).toLong()
+                val count = (highSpeed - lowSpeed + 1).coerceAtLeast(0L)
+                number *= count
+            } else {
+                number = 0L
             }
-            number *= counter
         }
         return number
     }
@@ -39,13 +45,13 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     var testInput = readInput("y2023", "Day06_test_part1")
-        println(part1(testInput))
+    println(part1(testInput))
 //    check(part1(testInput) == 35)
 //    println(part2(testInput))
 //    check(part2(testInput) == 2286)
 
     val input = readInput("y2023", "Day06")
-        println(part1(input))
+    println(part1(input))
 //    check(part1(input) == 2563)
 //    check(part2(input) == 70768)
 //    println(part2(input))
