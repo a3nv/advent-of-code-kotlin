6ebd6157b427ccaaf50c20b20bdc216be656b2f0
Ivan Asonov <asonovivan88@gmail.com>
2024-12-01 21:13:14 +0100

    Updated kotlin and gradle versions;

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.7.22"
+    kotlin("jvm") version "2.1.0"
 }
 
 repositories {
@@ -14,6 +14,6 @@ tasks {
     }
 
     wrapper {
-        gradleVersion = "7.6"
+        gradleVersion = "8.11.1"
     }
 }
